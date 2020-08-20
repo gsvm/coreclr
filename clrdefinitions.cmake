dotnet/runtime

commit 1e7552afb359d1fba2dc57d6f82f03d20db32dd2

Fix LTTng build for build environments with older liblttng-ust-dev  (#27273)
* Fix macro redefinition to use XplatEventLogger instead of simply writing FALSE

* Fix linker error

* undo newline changes

* Some changes to comment

* Move wrapper export from eventpipe.cpp to eventtrace.cpp

src/scripts/genLttngProvider.py
@@ -546,7 +546,10 @@ def generateLttngFiles(etwmanifest,eventprovider_directory, dryRun):
                 lttngimpl_file.write("#include \"" + lttngevntheadershortname + "\"\n\n")
 
                 lttngimpl_file.write("""#ifndef tracepoint_enabled
-#define tracepoint_enabled(provider, name) FALSE
+
+extern "C" bool XplatEventLoggerIsEnabled();
+
+#define tracepoint_enabled(provider, name) XplatEventLoggerIsEnabled()
 #define do_tracepoint tracepoint
 #endif
 

src/vm/eventtrace.cpp
@@ -7540,3 +7540,11 @@ bool EventPipeHelper::IsEnabled(DOTNET_TRACE_CONTEXT Context, UCHAR Level, ULONG
     return false;
 }
 #endif // FEATURE_PERFTRACING
+
+#if defined(FEATURE_PAL)  && defined(FEATURE_PERFTRACING)
+// This is a wrapper method for LTTng. See https://github.com/dotnet/coreclr/pull/27273 for details.
+extern "C" bool XplatEventLoggerIsEnabled()
+{
+    return XplatEventLogger::IsEventLoggingEnabled();
+}
+#endif // FEATURE_PAL && FEATURE_PERFTRACING

gsvm/coreclr

commit 5c06a51bbdbbb61a338aeb9d13c210df4d18ec26

Sync-20200820 (#25)
* 14237: Fix Assertion failed 'padding == sizeof(int)' issue

* 14049: mult, multu, div, divu need sign-extend Operands

* 14282: revert Fix LTTng build for build environments with older liblttng-ust-dev

https://github.com/dotnet/coreclr/commit/1e7552afb359d1fba2dc57d6f82f03d20db32dd2 caused failure of crossbuild for mips64

* 13849: Fix GC refs type in genCodeForCpObj.

* 14238: [MIPS64] Fix assert error when GC=4 for gc-stacklcl.

* 14049: Replace addu with sll for sign-extend

* 14210: Add the definition of FEATURE_USE_SOFTWARE_WRITE_WATCH_FOR_GC_HEAP for MIPS64

Co-authored-by: qiaopengcheng <qiaopengcheng-hf@loongson.cn>
Co-authored-by: wanghaomin <wanghaomin@loongson.cn>
Co-authored-by: wangrui <wangrui@loongson.cn>

clrdefinitions.cmake
@@ -226,9 +226,9 @@ if(CLR_CMAKE_PLATFORM_UNIX AND CLR_CMAKE_TARGET_ARCH_AMD64)
   add_definitions(-DUNIX_AMD64_ABI)
 endif(CLR_CMAKE_PLATFORM_UNIX AND CLR_CMAKE_TARGET_ARCH_AMD64)
 add_definitions(-DFEATURE_USE_ASM_GC_WRITE_BARRIERS)
-if(CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64)
+if(CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64 OR CLR_CMAKE_PLATFORM_ARCH_MIPS64)
   add_definitions(-DFEATURE_USE_SOFTWARE_WRITE_WATCH_FOR_GC_HEAP)
-endif(CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64)
+endif(CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64 OR CLR_CMAKE_PLATFORM_ARCH_MIPS64)
 if((CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64) AND NOT WIN32)
   add_definitions(-DFEATURE_MANUALLY_MANAGED_CARD_BUNDLES)
 endif((CLR_CMAKE_PLATFORM_ARCH_AMD64 OR CLR_CMAKE_PLATFORM_ARCH_ARM64) AND NOT WIN32)

src/jit/codegencommon.cpp
@@ -3935,7 +3935,7 @@ void CodeGen::genFnPrologCalleeRegArgs(regNumber xtraReg, bool* pXtraRegClobbere
 
             if ((-32768 <= base) && (base <= 32767))
             {
-                getEmitter()->emitIns_R_R_I(ins_Store(storeType), size, srcRegNum, reg2, base);
+                getEmitter()->emitIns_S_R(ins_Store(storeType), size, srcRegNum, varNum, baseOffset);
             }
             else
             {
@@ -3946,11 +3946,12 @@ void CodeGen::genFnPrologCalleeRegArgs(regNumber xtraReg, bool* pXtraRegClobbere
                     getEmitter()->emitIns_R_I(INS_lui, EA_PTRSIZE, REG_AT, tmp_offset>>16);
                     getEmitter()->emitIns_R_R_I(INS_ori, EA_PTRSIZE, REG_AT, REG_AT, tmp_offset & 0xffff);
                     getEmitter()->emitIns_R_R_R(INS_daddu, EA_PTRSIZE, REG_AT, REG_AT, reg2);
-                    getEmitter()->emitIns_R_R_I(ins_Store(storeType), size, srcRegNum, REG_AT, 0);
+                    getEmitter()->emitIns_S_R(ins_Store(storeType), size, srcRegNum, varNum, -8);
                 }
                 else
                 {
-                    getEmitter()->emitIns_R_R_I(ins_Store(storeType), size, srcRegNum, REG_AT, base - tmp_offset);
+                    baseOffset = -(base - tmp_offset) - 8;
+                    getEmitter()->emitIns_S_R(ins_Store(storeType), size, srcRegNum, varNum, baseOffset);
                 }
             }
 
@@ -7141,7 +7142,6 @@ void CodeGen::genZeroInitFrame(int untrLclHi, int untrLclLo, regNumber initReg, 
         if (uCntBytes > 0)
         {
             assert(uCntBytes == sizeof(int));
-            assert(padding == sizeof(int));
             getEmitter()->emitIns_R_R_I(INS_sw, EA_4BYTE, REG_R0, rAddr, padding);
             uCntBytes -= sizeof(int);
         }

src/jit/codegenmips64.cpp
@@ -1708,10 +1708,14 @@ void CodeGen::genCodeForMulHi(GenTreeOp* treeNode)
     else
     {
         assert(EA_SIZE(attr) == EA_4BYTE);
+        regNumber tmpRegOp1 = treeNode->ExtractTempReg();
+        regNumber tmpRegOp2 = treeNode->ExtractTempReg();
+        emit->emitIns_R_R_I(INS_sll, attr, tmpRegOp1, op1->gtRegNum, 0);
+        emit->emitIns_R_R_I(INS_sll, attr, tmpRegOp2, op2->gtRegNum, 0);
 
         instruction ins = isUnsigned ? INS_multu : INS_mult;
 
-        emit->emitIns_R_R(ins, attr, op1->gtRegNum, op2->gtRegNum);
+        emit->emitIns_R_R(ins, attr, tmpRegOp1, tmpRegOp2);
 
         emit->emitIns_R(INS_mfhi, attr, targetReg);
     }
@@ -2330,7 +2334,7 @@ void CodeGen::genCodeForNegNot(GenTree* tree)
     if (ins == INS_not && attr == EA_4BYTE)
     {
         // MIPS needs to sign-extend dst when deal with 32bit data
-        getEmitter()->emitIns_R_R_R(INS_addu, attr, targetReg, targetReg, REG_R0);
+        getEmitter()->emitIns_R_R_I(INS_sll, attr, targetReg, targetReg, 0);
     }
 
     genProduceReg(tree);
@@ -2677,6 +2681,8 @@ void CodeGen::genCodeForCpObj(GenTreeObj* cpObjNode)
     emitter* emit = getEmitter();
 
     BYTE* gcPtrs = cpObjNode->gtGcPtrs;
+    emitAttr attrSrcAddr = emitActualTypeSize(srcAddrType);
+    emitAttr attrDstAddr = emitActualTypeSize(dstAddr->TypeGet());
 
     // If we can prove it's on the stack we don't need to use the write barrier.
     if (dstOnStack)
@@ -2689,11 +2695,11 @@ void CodeGen::genCodeForCpObj(GenTreeObj* cpObjNode)
             emitAttr attr1 = emitTypeSize(compiler->getJitGCType(gcPtrs[i + 1]));
 
             emit->emitIns_R_R_I(INS_ld, attr0, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, 0);
-            emit->emitIns_R_R_I(INS_ld, attr0, tmpReg2, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
-            emit->emitIns_R_R_I(INS_daddiu, attr1, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, 2 * TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_ld, attr1, tmpReg2, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_daddiu, attrSrcAddr, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, 2 * TARGET_POINTER_SIZE);
             emit->emitIns_R_R_I(INS_sd, attr0, tmpReg, REG_WRITE_BARRIER_DST_BYREF, 0);
-            emit->emitIns_R_R_I(INS_sd, attr0, tmpReg2, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
-            emit->emitIns_R_R_I(INS_daddiu, attr1, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, 2 * TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_sd, attr1, tmpReg2, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_daddiu, attrDstAddr, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, 2 * TARGET_POINTER_SIZE);
             i += 2;
         }
 
@@ -2703,9 +2709,9 @@ void CodeGen::genCodeForCpObj(GenTreeObj* cpObjNode)
             emitAttr attr0 = emitTypeSize(compiler->getJitGCType(gcPtrs[i + 0]));
 
             emit->emitIns_R_R_I(INS_ld, attr0, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, 0);
-            emit->emitIns_R_R_I(INS_daddiu, attr0, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_daddiu, attrSrcAddr, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
             emit->emitIns_R_R_I(INS_sd, attr0, tmpReg, REG_WRITE_BARRIER_DST_BYREF, 0);
-            emit->emitIns_R_R_I(INS_daddiu, attr0, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
+            emit->emitIns_R_R_I(INS_daddiu, attrDstAddr, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
         }
     }
     else
@@ -2723,18 +2729,18 @@ void CodeGen::genCodeForCpObj(GenTreeObj* cpObjNode)
                     {
                         emit->emitIns_R_R_I(INS_ld, EA_8BYTE, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, 0);
                         emit->emitIns_R_R_I(INS_ld, EA_8BYTE, tmpReg2, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
-                        emit->emitIns_R_R_I(INS_daddiu, EA_8BYTE, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, 2 * TARGET_POINTER_SIZE);
+                        emit->emitIns_R_R_I(INS_daddiu, attrSrcAddr, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, 2 * TARGET_POINTER_SIZE);
                         emit->emitIns_R_R_I(INS_sd, EA_8BYTE, tmpReg, REG_WRITE_BARRIER_DST_BYREF, 0);
                         emit->emitIns_R_R_I(INS_sd, EA_8BYTE, tmpReg2, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
-                        emit->emitIns_R_R_I(INS_daddiu, EA_8BYTE, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, 2 * TARGET_POINTER_SIZE);
+                        emit->emitIns_R_R_I(INS_daddiu, attrDstAddr, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, 2 * TARGET_POINTER_SIZE);
                         ++i; // extra increment of i, since we are copying two items
                     }
                     else
                     {
                         emit->emitIns_R_R_I(INS_ld, EA_8BYTE, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, 0);
-                        emit->emitIns_R_R_I(INS_daddiu, EA_8BYTE, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
+                        emit->emitIns_R_R_I(INS_daddiu, attrSrcAddr, REG_WRITE_BARRIER_SRC_BYREF, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE);
                         emit->emitIns_R_R_I(INS_sd, EA_8BYTE, tmpReg, REG_WRITE_BARRIER_DST_BYREF, 0);
-                        emit->emitIns_R_R_I(INS_daddiu, EA_8BYTE, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
+                        emit->emitIns_R_R_I(INS_daddiu, attrDstAddr, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE);
                     }
                     break;
 
@@ -4041,7 +4047,7 @@ void CodeGen::genCodeForCompare(GenTreeOp* tree)
                 if (IsUnsigned)
                     emit->emitIns_R_R_I_I(INS_dext, EA_PTRSIZE, tmpRegOp1, op1->gtRegNum, 0, 32);
                 else
-                    emit->emitIns_R_R_R(INS_addu, EA_PTRSIZE, tmpRegOp1, op1->gtRegNum, REG_R0);
+                    emit->emitIns_R_R_I(INS_sll, EA_4BYTE, tmpRegOp1, op1->gtRegNum, 0);
             }
 
             if (tree->OperIs(GT_CMP))
@@ -4138,8 +4144,8 @@ void CodeGen::genCodeForCompare(GenTreeOp* tree)
                 }
                 else
                 {
-                    emit->emitIns_R_R_R(INS_addu, EA_PTRSIZE, tmpRegOp1, op1->gtRegNum, REG_R0);
-                    emit->emitIns_R_R_R(INS_addu, EA_PTRSIZE, tmpRegOp2, op2->gtRegNum, REG_R0);
+                    emit->emitIns_R_R_I(INS_sll, EA_4BYTE, tmpRegOp1, op1->gtRegNum, 0);
+                    emit->emitIns_R_R_I(INS_sll, EA_4BYTE, tmpRegOp2, op2->gtRegNum, 0);
                 }
             }
 
@@ -9449,7 +9455,7 @@ void CodeGen::genIntToIntCast(GenTreeCast* cast)
     //    if (dstType == TYP_INT)
     //    {
     //        // convert t0 int32
-    //        emit->emitIns_R_R_I(INS_addiu, EA_4BYTE, dstReg, srcReg, 0);
+    //        emit->emitIns_R_R_I(INS_sll, EA_4BYTE, dstReg, srcReg, 0);
     //    }
     //    else
     //    {
@@ -9484,7 +9490,7 @@ void CodeGen::genIntToIntCast(GenTreeCast* cast)
                 emit->emitIns_R_R_I_I(INS_dext, EA_PTRSIZE, dstReg, srcReg, pos, 32);
                 break;
             case GenIntCastDesc::SIGN_EXTEND_INT:
-                emit->emitIns_R_R_I(INS_addiu, EA_4BYTE, dstReg, srcReg, 0);
+                emit->emitIns_R_R_I(INS_sll, EA_4BYTE, dstReg, srcReg, 0);
                 break;
 #endif
             default:

src/jit/emit.cpp
@@ -3941,7 +3941,7 @@ AGAIN:
             //More Info see emitter::emitIns_R_C() within file emitmips64.cpp
             assert(jmpDist > 0);
             assert(!(jmpDist & 0x3));
-            jmp->idAddr()->iiaSetInstrEncode((unsigned int)jmpDist);//temporarily saved data's offset.
+            jmp->idAddr()->iiaSetJmpOffset((unsigned int)jmpDist);//temporarily saved data's offset.
 
             continue;
         }
@@ -4106,7 +4106,7 @@ AGAIN:
 #if defined(_TARGET_MIPS64_)
             assert(jmpDist >= 0);//Forward jump
             assert(!(jmpDist & 0x3));
-            jmp->idAddr()->iiaSetInstrEncode((unsigned int)jmpDist);
+            jmp->idAddr()->iiaSetJmpOffset((unsigned int)jmpDist);
 #endif
             if (extra <= 0)
             {
@@ -4125,7 +4125,7 @@ AGAIN:
 #if defined(_TARGET_MIPS64_)
             assert(jmpDist >= 0);//Backward jump
             assert(!(jmpDist & 0x3));
-            jmp->idAddr()->iiaSetInstrEncode((unsigned int)jmpDist+1);//NOTE:bit0=1 is Backward jump!
+            jmp->idAddr()->iiaSetJmpOffset((unsigned int)jmpDist+1);//NOTE:bit0=1 is Backward jump!
 #endif
 
             /* How much beyond the max. short distance does the jump go? */

src/jit/emit.h
@@ -732,8 +732,8 @@ protected:
 #ifdef _TARGET_MIPS64_
         /* FIXME for MIPS: maybe delete on future. */
         opSize   _idOpSize : 3; // operand size: 0=1 , 1=2 , 2=4 , 3=8, 4=16
-        insOpts  _idInsOpt : 6; // mips options for special: placeholders. e.g emitIns_R_C.
-        unsigned _idLclVar : 1; // access a local on stack
+        insOpts  _idInsOpt : 6; // mips options for special: placeholders. e.g emitIns_R_C, also identifying the accessing a local on stack.
+        unsigned _idLclVar : 1; // access a local on stack, TODO: will be delelted later !!!
 #endif
 
 #ifdef _TARGET_ARM_
@@ -859,9 +859,9 @@ protected:
 // about reading what we think is here, to avoid unexpected corruption issues.
 
 #if !defined(_TARGET_ARM64_) && !defined(_TARGET_MIPS64_)
-/* FIXME for MIPS: maybe needed this for mips. */
             emitLclVarAddr iiaLclVar;
 #endif
+
             BasicBlock*  iiaBBlabel;
             insGroup*    iiaIGlabel;
             BYTE*        iiaAddr;
@@ -915,14 +915,18 @@ protected:
 
             struct
             {
-                // For 64-bit architecture this 32-bit structure can pack with these unsigned bit fields
-                emitLclVarAddr iiaLclVar;
-                GCtype         _idGCref2 : 2;
+                unsigned int iiaEncodedInstr;//instruction's binary encoding.
+                //GCtype         _idGCref2 : 2;
                 regNumber _idReg3 : REGNUM_BITS;
                 regNumber _idReg4 : REGNUM_BITS;
             };
 
-            unsigned int iiaEncodedInstr;//instruction's binary encoding or jmp's temporary offset.
+            struct
+            {
+                unsigned int iiaJmpOffset;//temporary saving the offset of jmp or data.
+                emitLclVarAddr iiaLclVar;
+            };
+
             void iiaSetInstrEncode(unsigned int encode)
             {
                 iiaEncodedInstr = encode;
@@ -930,6 +934,15 @@ protected:
             unsigned int iiaGetInstrEncode() const
             {
                 return iiaEncodedInstr;
+            }
+
+            void iiaSetJmpOffset(unsigned int offset)
+            {
+                iiaJmpOffset = offset;
+            }
+            unsigned int iiaGetJmpOffset() const
+            {
+                return iiaJmpOffset;
             }
 #endif // defined(_TARGET_MIPS64_)
 
@@ -1093,17 +1106,17 @@ protected:
 #endif // _TARGET_ARM64_
 
 #ifdef _TARGET_MIPS64_
-        /* FIXME for MIPS: is needed for mips64? */
-        GCtype idGCrefReg2() const
-        {
-            assert(!idIsSmallDsc());
-            return (GCtype)idAddr()->_idGCref2;
-        }
-        void idGCrefReg2(GCtype gctype)
-        {
-            assert(!idIsSmallDsc());
-            idAddr()->_idGCref2 = gctype;
-        }
+        ///* FIXME for MIPS: is needed for mips64? */
+        //GCtype idGCrefReg2() const
+        //{
+        //    assert(!idIsSmallDsc());
+        //    return (GCtype)idAddr()->_idGCref2;
+        //}
+        //void idGCrefReg2(GCtype gctype)
+        //{
+        //    assert(!idIsSmallDsc());
+        //    idAddr()->_idGCref2 = gctype;
+        //}
 #endif // _TARGET_MIPS64_
 
         regNumber idReg2() const

src/jit/emitmips64.cpp
@@ -1912,12 +1912,15 @@ void emitter::emitIns(instruction ins)
 
 /*****************************************************************************
  *
- *  Add an Load/Store instruction: base+offset.
+ *  Add an Load/Store instruction(s): base+offset and base-addr-computing if needed.
+ *  For referencing a stack-based local variable and a register
  */
 void emitter::emitIns_S_R(instruction ins, emitAttr attr, regNumber reg1, int varx, int offs)
 {
-    assert(offs >= 0);
+    //assert(offs >= 0);
     ssize_t imm;
+    regNumber regs[2];
+    regs[0] = reg1;
 
     emitAttr  size  = EA_SIZE(attr);//it's better confirm attr with ins.
 
@@ -1930,9 +1933,11 @@ void emitter::emitIns_S_R(instruction ins, emitAttr attr, regNumber reg1, int va
             break;
         case INS_sw:
         case INS_swc1:
-        //case INS_swl:
-        //case INS_swr:
+        case INS_swl:
+        case INS_swr:
             break;
+        case INS_sdl:
+        case INS_sdr:
         case INS_sd:
         case INS_sdc1:
             break;
@@ -1949,13 +1954,15 @@ void emitter::emitIns_S_R(instruction ins, emitAttr attr, regNumber reg1, int va
     bool FPbased;
 
     base = emitComp->lvaFrameAddress(varx, &FPbased);
-    imm = base + offs;
+    imm = offs < 0 ? -offs -8: base + offs;
 
     regNumber reg2 = FPbased ? REG_FPBASE : REG_SPBASE;
+    reg2 = offs < 0 ? REG_AT : reg2;
+    offs = offs < 0 ? -offs -8: offs;
 
     if ((-32768 <= imm) && (imm <= 32767))
     {
-        emitIns_R_R_I(ins, attr, reg1, reg2, imm);
+        regs[1] = reg2;
     }
     else
     {
@@ -1964,16 +1971,34 @@ void emitter::emitIns_S_R(instruction ins, emitAttr attr, regNumber reg1, int va
         emitIns_R_I(INS_lui, EA_PTRSIZE, REG_AT, imm2);
         imm2 = imm & 0xffff;
         emitIns_R_R_I(INS_ori, EA_PTRSIZE, REG_AT, REG_AT, imm2);
-        emitIns_R_R_R(INS_daddu, EA_8BYTE, REG_AT, REG_AT, reg2);
-        emitIns_R_R_I(ins, attr, reg1, REG_AT, 0);
+        emitIns_R_R_R(INS_daddu, attr, REG_AT, REG_AT, reg2);
+
+        regs[1] = REG_AT;
+        reg2 = REG_AT;
+        imm = 0;
     }
 
+    instrDesc* id = emitNewInstr(attr);
+
+    id->idReg1(reg1);
+
+    id->idReg2(reg2);
+
+    id->idIns(ins);
+    id->idAddr()->iiaSetInstrEncode(emitInsOps(ins, regs, &imm));
+    id->idAddr()->iiaLclVar.initLclVarAddr(varx, offs);
+    id->idSetIsLclVar();
+
+    //dispIns(id);
+    appendToCurIG(id);
 }
 
 void emitter::emitIns_R_S(instruction ins, emitAttr attr, regNumber reg1, int varx, int offs)
 {
     assert(offs >= 0);
     ssize_t imm;
+    regNumber regs[3];
+    regs[0] = reg1;
 
     emitAttr  size  = EA_SIZE(attr);//it's better confirm attr with ins.
 
@@ -1990,12 +2015,16 @@ void emitter::emitIns_R_S(instruction ins, emitAttr attr, regNumber reg1, int va
         case INS_lhu:
             break;
 
+        case INS_lwl:
+        case INS_lwr:
         case INS_sw:
         case INS_lw:
         case INS_lwu:
         case INS_lwc1:
             break;
 
+        case INS_ldl:
+        case INS_ldr:
         case INS_sd:
         case INS_ld:
         case INS_ldc1:
@@ -2029,8 +2058,7 @@ void emitter::emitIns_R_S(instruction ins, emitAttr attr, regNumber reg1, int va
         {
             ins = INS_daddiu;
         }
-
-        emitIns_R_R_I(ins, attr, reg1, reg2, imm);
+        regs[1] = reg2;
     }
     else
     {
@@ -2039,16 +2067,34 @@ void emitter::emitIns_R_S(instruction ins, emitAttr attr, regNumber reg1, int va
         emitIns_R_I(INS_lui, EA_PTRSIZE, REG_AT, imm2);
         imm2 = imm & 0xffff;
         emitIns_R_R_I(INS_ori, EA_PTRSIZE, REG_AT, REG_AT, imm2);
+
+        regs[1] = REG_AT;
         if (ins == INS_lea)
         {
-            emitIns_R_R_R(INS_daddu, EA_8BYTE, reg1, REG_AT, reg2);
+            regs[2] = reg2;
+            ins = INS_daddu;
+            //id->idReg3(reg2);//NOTE: in fact not used. so delete.
         }
         else
         {
-            emitIns_R_R_R(INS_daddu, EA_8BYTE, REG_AT, REG_AT, reg2);
-            emitIns_R_R_I(ins, attr, reg1, REG_AT, 0);
+            emitIns_R_R_R(INS_daddu, attr, REG_AT, REG_AT, reg2);
+            imm = 0;
         }
+        reg2 = REG_AT;
     }
+
+    instrDesc* id = emitNewInstr(attr);
+
+    id->idReg1(reg1);
+    id->idReg2(reg2);
+
+    id->idIns(ins);
+    id->idAddr()->iiaSetInstrEncode(emitInsOps(ins, regs, &imm));
+    id->idAddr()->iiaLclVar.initLclVarAddr(varx, offs);
+    id->idSetIsLclVar();
+
+    //dispIns(id);
+    appendToCurIG(id);
 }
 
 /*****************************************************************************
@@ -4776,7 +4822,7 @@ size_t emitter::emitOutputInstr(insGroup* ig, instrDesc* id, BYTE** dp)
             *(code_t *)dst = emitInsOps(INS_bal, nullptr, &imm);
             dst += 4;
 
-            unsigned int dataOffs = jmp->idAddr()->iiaGetInstrEncode();//get data's offset.
+            unsigned int dataOffs = jmp->idAddr()->iiaGetJmpOffset();//get data's offset.
             assert(dataOffs > 0);
             assert(!(dataOffs & 0x3));
 
@@ -4826,7 +4872,7 @@ size_t emitter::emitOutputInstr(insGroup* ig, instrDesc* id, BYTE** dp)
             *(code_t *)dst = emitInsOps(INS_bal, nullptr, &imm);
             dst += 4;
 
-            imm = (ssize_t) id->idAddr()->iiaGetInstrEncode();//get jmp's offset, temporarily saved.
+            imm = (ssize_t) id->idAddr()->iiaGetJmpOffset();//get jmp's offset, temporarily saved.
             assert(imm > 0);
 
             //NOTE:bit0=1 is Backward jump!
@@ -4888,7 +4934,7 @@ size_t emitter::emitOutputInstr(insGroup* ig, instrDesc* id, BYTE** dp)
         //   jalr/jr  t9
         //   nop           <-----here add delay-slot!
         {
-            ssize_t imm = (ssize_t) id->idAddr()->iiaGetInstrEncode();//get jmp's offset, temporarily saved.
+            ssize_t imm = (ssize_t) id->idAddr()->iiaGetJmpOffset();//get jmp's offset, temporarily saved.
             assert(imm >= 0);
 
             //NOTE:bit0=1 is Backward jump!
@@ -8533,9 +8579,19 @@ regNumber emitter::emitInsTernary(instruction ins, emitAttr attr, GenTree* dst, 
     else if (dst->OperGet() == GT_MUL)
     {
         ssize_t imm;
-
+        if (EA_SIZE(attr) == EA_4BYTE)
+        {
+            regNumber tmpRegOp1 = dst->ExtractTempReg();
+            regNumber tmpRegOp2 = dst->ExtractTempReg();
+            emitIns_R_R_I(INS_sll, attr, tmpRegOp1, src1->gtRegNum, 0);
+            emitIns_R_R_I(INS_sll, attr, tmpRegOp2, src2->gtRegNum, 0);
+            emitIns_R_R(ins, attr, tmpRegOp1, tmpRegOp2);
+        }
+        else
+        {
+            emitIns_R_R(ins, attr, src1->gtRegNum, src2->gtRegNum);
+        }
         // n * n bytes will store n bytes result
-        emitIns_R_R(ins, EA_SIZE(attr), src1->gtRegNum, src2->gtRegNum);
         emitIns_R(INS_mflo, attr, dst->gtRegNum);
 
         if (needCheckOv)
@@ -8571,7 +8627,18 @@ regNumber emitter::emitInsTernary(instruction ins, emitAttr attr, GenTree* dst, 
     }
     else if (dst->OperGet() == GT_DIV || dst->OperGet() == GT_UDIV || dst->OperGet() == GT_MOD || dst->OperGet() == GT_UMOD)
     {
-        emitIns_R_R(ins, EA_SIZE(attr), src1->gtRegNum, src2->gtRegNum);
+        if (EA_SIZE(attr) == EA_4BYTE)
+        {
+            regNumber tmpRegOp1 = dst->ExtractTempReg();
+            regNumber tmpRegOp2 = dst->ExtractTempReg();
+            emitIns_R_R_I(INS_sll, attr, tmpRegOp1, src1->gtRegNum, 0);
+            emitIns_R_R_I(INS_sll, attr, tmpRegOp2, src2->gtRegNum, 0);
+            emitIns_R_R(ins, attr, tmpRegOp1, tmpRegOp2);
+        }
+        else
+        {
+            emitIns_R_R(ins, attr, src1->gtRegNum, src2->gtRegNum);
+        }
 
         if (dst->OperGet() == GT_DIV || dst->OperGet() == GT_UDIV)
             emitIns_R(INS_mflo, attr, dst->gtRegNum);
@@ -8586,7 +8653,7 @@ regNumber emitter::emitInsTernary(instruction ins, emitAttr attr, GenTree* dst, 
 
         // MIPS needs to sign-extend dst when deal with 32bit data
         if (attr == EA_4BYTE)
-             emitIns_R_R_R(INS_addu, attr, dst->gtRegNum, dst->gtRegNum, REG_R0);
+             emitIns_R_R_I(INS_sll, attr, dst->gtRegNum, dst->gtRegNum, 0);
     }
     else
     {

src/jit/lsramips64.cpp
@@ -296,20 +296,19 @@ int LinearScan::BuildNode(GenTree* tree)
         //    break;
 
         case GT_MUL:
-            if (tree->gtOverflow())
-            {
-                // Need a register different from target reg to check for overflow.
-                buildInternalIntRegisterDefForNode(tree);
-                setInternalRegsDelayFree = true;
-            }
-            __fallthrough;
-
         case GT_MOD:
         case GT_UMOD:
         case GT_DIV:
         case GT_MULHI:
         case GT_UDIV:
         {
+            if (emitActualTypeSize(tree) == EA_4BYTE)
+            {
+                // We need two registers: tmpRegOp1 and tmpRegOp2
+                buildInternalIntRegisterDefForNode(tree);
+                buildInternalIntRegisterDefForNode(tree);
+            }
+
             srcCount = BuildBinaryUses(tree->AsOp());
             buildInternalRegisterUses();
             assert(dstCount == 1);

src/scripts/genLttngProvider.py
@@ -546,10 +546,7 @@ def generateLttngFiles(etwmanifest,eventprovider_directory, dryRun):
                 lttngimpl_file.write("#include \"" + lttngevntheadershortname + "\"\n\n")
 
                 lttngimpl_file.write("""#ifndef tracepoint_enabled
-
-extern "C" bool XplatEventLoggerIsEnabled();
-
-#define tracepoint_enabled(provider, name) XplatEventLoggerIsEnabled()
+#define tracepoint_enabled(provider, name) FALSE
 #define do_tracepoint tracepoint
 #endif
 

src/vm/eventtrace.cpp
@@ -7543,11 +7543,3 @@ bool EventPipeHelper::IsEnabled(DOTNET_TRACE_CONTEXT Context, UCHAR Level, ULONG
     return false;
 }
 #endif // FEATURE_PERFTRACING
-
-#if defined(FEATURE_PAL)  && defined(FEATURE_PERFTRACING)
-// This is a wrapper method for LTTng. See https://github.com/dotnet/coreclr/pull/27273 for details.
-extern "C" bool XplatEventLoggerIsEnabled()
-{
-    return XplatEventLogger::IsEventLoggingEnabled();
-}
-#endif // FEATURE_PAL && FEATURE_PERFTRACING